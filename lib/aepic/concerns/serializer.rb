8df64b565f937f6f009e11d9761ffeaf877744cf
Alexander Semyonov <al@semyonov.us>
2013-09-06 20:21:12 -0700

    Better defaults for JSON-API compatibility

--- lib/aepic/concerns/serializer.rb
+++ lib/aepic/concerns/serializer.rb
@@ -6,31 +6,49 @@ module Aepic
     module Serializer
       extend ActiveSupport::Concern
 
-      XSD_TYPES = Hash.new { |hash, key| hash[key] = "xsd:#{key}" }.merge({
+      XSD_TYPES = Hash.new do |hash, key|
+        hash[key] = "xsd:#{key}"
+      end.merge({
       })
 
       included do
         attributes :id
+        embed :ids, include: true
       end
 
       module ClassMethods
+        #def inherited(child)
+        #  super
+        #  child.root = child.name.gsub(/Serializer$/, '').pluralize.camelcase(:lower)
+        #end
+
         def jsonld_context
           {}.tap do |context|
             schema[:attributes].each do |name, type|
               context[name] = XSD_TYPES[type]
             end
+            schema[:associations].each do |name, type|
+              associated_class = "#{name.to_s.classify}Decorator".constantize
+              context[name] = associated_class.jsonld_context
+            end
           end
         end
 
         def jsonld
-          {'@context' => jsonld_context.merge(xsd: 'http://www.w3.org/2001/XMLSchema#')}
+          {'@context' => {xsd: 'http://www.w3.org/2001/XMLSchema#'}.merge(jsonld_context)}
         end
       end
 
+      #def root_name
+      #  self.class._root
+      #end
+
       def attributes
-        hash = super
-        hash['@context'] = object.jsonld_context
-        hash
+        {'@context' => jsonld_context}.merge(super)
+      end
+
+      def jsonld_context
+        object.jsonld_context
       end
     end
   end
